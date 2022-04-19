9cd51367e657f356b59dcd33a43e56c0e615f78c
Melissa Osorio Tavera <melissa.osorio.tavera@gmail.com>
2022-04-19 14:09:04 -0500

    Lab terminado

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app=express();
+
+app.use(express.static('public'))
+
+app.get('/', (req,res,next)=>{
+    res.sendFile(__dirname+'/views/home.html')
+})
+
+app.get('/about', (req,res,next)=>{
+    res.sendFile(__dirname+'/views/about.html')
+})
+
+app.get('/works', (req,res,next)=>{
+    res.sendFile(__dirname+'/views/works.html')
+})
+
+app.listen(3000,()=>{
+    console.log("Estoy corriendo en el puerto 3000")
+})
